8a5ed24cb452d8c7fac5fc4735a65158512bdcf3
Priyadharshini S <priyadharshinis2324it@psnacet.edu.in>
2026-07-25 23:06:30 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,12 @@
+public class odd {
+    public static void main(String args[]){
+        int num=5;
+        if(num%2==0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    
+}
