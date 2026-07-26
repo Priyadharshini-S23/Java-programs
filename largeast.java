ad0fe1e0f36e7c07840ca33f421b3d726f6e3ff0
Priyadharshini S <priyadharshinis2324it@psnacet.edu.in>
2026-07-26 22:28:54 +0530

    Add files via upload

---
+++ largeast.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class largeast {
+    public static void main(String agrs[]){
+        Scanner scan = new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        if(a>b && a>c){
+            System.out.println("a is greater");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is greater");
+        }
+        else{
+            System.out.println("c is greater");
+        }
+    }
+}
+    
